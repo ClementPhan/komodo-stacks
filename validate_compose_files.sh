d0c3b9cecba4bbd1828843aee716ebaba747c40b
Clement PHAN <9255146+ClementPhan@users.noreply.github.com>
2026-02-05 11:45:10 +0100

    Update validation script

--- validate_compose_files.sh
+++ validate_compose_files.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # Validate all compose.yaml files in the current directory and subdirectories.
-find . \( \( -type d -name .git -prune \) -o -type f \) -a -name 'compose.yaml' | while read file; do
-    echo "Validating $file"
+shopt -s globstar
+
+errors=""
+for file in stacks/**/compose.yaml; do
+    echo -n "Validating $file"
     OUTPUT="$(docker compose -f "$file" config 2>&1)"
-    if [ $? -ne 0 ]; then
-        echo "Validation failed for $file"
-        echo "$OUTPUT" | sed 's/^/>> /g'
-        exit 1
+    if [ $? -eq 0 ]; then
+        echo " - OK"
+    else
+        echo " - FAILED"
+        errors="${errors}\nValidation failed for $file\n$(echo "$OUTPUT" | sed 's/^/>> /g')\n"
     fi
 done
+
+if [ -n "$errors" ]; then
+    echo ""
+    echo "Validation errors found:"
+    echo -e "$errors"
+    exit 1
+else
+    echo "All compose files validated successfully."
+fi
